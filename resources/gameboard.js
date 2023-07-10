98fc8a31bbf9fc07a4b3224c34477fb71a86063a
Hikmat Ali <hikmat.aliyevm@gmail.com>
2023-07-10 11:35:35 +0400

    Create receive attack function

--- resources/gameboard.js
+++ resources/gameboard.js
@@ -2,61 +2,80 @@ import Ship from "./ship";
 
 export default function Gameboard() {
     const boardSize = 10;
-    const board = new Array(boardSize * boardSize).fill(null);
+    const board = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
     const ships = [];
-  
-    function placeShip(shipLength, coordinates) {
-      const ship = Ship(shipLength);
 
-      if (!isValidPlacement(ship, coordinates)) {
-        return false;
-      }
-  
-      coordinates.forEach((coordinate) => {
-        const [row, col] = coordinate;
-        const index = row * boardSize + col;
-        board[index] = ship;
-      });
-  
-      ships.push(ship);
-      return true;
-    }
-  
-    function isValidPlacement(ship, coordinates) {
-      const [rowStart, colStart] = coordinates[0];
-      const [rowEnd, colEnd] = coordinates[coordinates.length - 1];
-  
-      // Check if ship is within the bounds of the board
-      if (
-        rowStart < 0 ||
-        rowStart >= boardSize ||
-        colStart < 0 ||
-        colStart >= boardSize ||
-        rowEnd < 0 ||
-        rowEnd >= boardSize ||
-        colEnd < 0 ||
-        colEnd >= boardSize
-      ) {
-        return false;
-      }
-  
-      // Check if any coordinates are already occupied by a ship
-      for (let i = 0; i < coordinates.length; i++) {
-        const [row, col] = coordinates[i];
-        const index = row * boardSize + col;
-        if (board[index] !== null) {
-          return false;
+    function placeShip(shipLength, rowStart, columnStart, isVertical) {
+        const ship = Ship(shipLength);
+        ships.push(ship);
+
+        if(isValidPlacement(shipLength, rowStart, columnStart, isVertical) === false) {
+            return false;
         }
-      }
-  
-      return true;
+
+        if(isVertical){
+            for(let i = 0; i < shipLength; i++) {
+                board[rowStart + i][columnStart] = ship;
+            }
+        }else {
+            for(let i = 0; i < shipLength; i++){
+                board[rowStart][columnStart + i] = ship;
+            }
+        }
+        return true;
     }
-  
-    // Other Gameboard methods...
-  
+
+    function isValidPlacement(shipLength, rowStart, columnStart, isVertical) {
+        let rowEnd;
+        let columnEnd;
+        
+        if(isVertical) {
+            rowEnd = rowStart + shipLength;
+            columnEnd = columnStart;
+        } else {
+            rowEnd = rowStart;
+            columnEnd = columnStart + shipLength;
+        }
+
+        // Check if ship is within the bounds of the board
+        if (
+            rowStart < 0 ||
+            rowStart >= boardSize ||
+            columnStart < 0 ||
+            columnStart >= boardSize ||
+            rowEnd < 0 ||
+            rowEnd >= boardSize ||
+            columnEnd < 0 ||
+            columnEnd >= boardSize
+          ) {
+            return false;
+          }
+
+          // Check if any coordinates are already occupied by a ship
+          if(board[rowStart][columnStart] !== null ||
+             board[rowEnd][columnEnd] !== null
+            ) {
+                return false;
+            }
+            return true;
+
+    }
+
+    function receiveAttack(row, column) {
+        //if the element is null, it's missed shot, change it to 0
+        if(board[row][column] === null) {
+            board[row][column] = 0;
+        //if element is already hit
+        } else if(board[row][column] === 0 || board[row][column] === 1){
+            return false;
+        } else {
+            board[row][column].hit();
+        }
+    }
+
     return {
-      placeShip,
-      // Other Gameboard methods...
-    };
-  }
-  
+        placeShip,
+        receiveAttack,
+        board: board
+    }
+}
