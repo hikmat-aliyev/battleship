7c4aca58328b56a751ff511d5701f241a383fe45
Hikmat Ali <hikmat.aliyevm@gmail.com>
2023-07-12 11:11:11 +0400

    Create test for player

---
+++ resources/tests/player.test.js
@@ -0,0 +1,19 @@
+import Gameboard from "../gameboard";
+import Player from "../player";
+import Ship from "../ship";
+
+
+it("dasda", () => {
+    expect(1).toBe(1)
+});
+
+it("computer attacks properly", () => {
+    const ship = Ship(3);
+    const computerGameboard = Gameboard();
+    computerGameboard.placeShip(ship.length, 1, 2, true);
+    const human = Player('human');
+    human.humanAttack(computerGameboard, 1, 2);
+    expect(human.humanAttack(computerGameboard, 1, 2)).toBe(false);
+    
+
+})
